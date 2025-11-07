88062a379239b7c17518b32d3016bf2195b0e06c
lilwizly <pl1022245@ahschool.com>
2025-11-07 08:42:43 -0500

    Add files via upload

--- Video_stream.py
+++ Video_stream.py
@@ -1,8 +1,11 @@
 import cv2
 
-cap = cv2.VideoCapture
+cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 if not cap.isOpened():
-  print("Error:Could not open video stream.")
+  print("Not open")
   exit()
 while True:
   ret, frame =cap.read()
